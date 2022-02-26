1b76d7c669ff2b7b59640bf44172622967a55495
ShadowsAdi <54354938+ShadowsAdi@users.noreply.github.com>
2022-02-26 16:23:00 +0200

    Update sv_main.cpp

--- rehlds/engine/sv_main.cpp
+++ rehlds/engine/sv_main.cpp
@@ -2366,9 +2366,9 @@ void EXT_FUNC SV_ConnectClient_internal(void)
 
 	int iNum = SV_CheckUserInfo(&adr, userinfo, reconnect, nClientSlot, name);
 
-	Con_Printf("here 3: %d\n", iNum);
-	if (!iNum)
-		return;
+	Con_Printf("here 3: %d | adr: %s | user: %s | rec: %d | slot:%d | name:%s\n", NET_AdrToString(adr), iNum, userinfo, reconnect, nClientSlot, name);
+	//if (!iNum)
+	return;
 
 	if (!SV_FinishCertificateCheck(&adr, nAuthProtocol, szRawCertificate, userinfo))
 		return;
